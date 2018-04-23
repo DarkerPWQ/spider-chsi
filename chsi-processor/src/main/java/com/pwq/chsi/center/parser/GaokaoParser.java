ca515e78f746d88c7ea9cabb92bb24d06bf1322c
puwenqiang <wenqiang_pu@xinyan.com>
2018-04-23 18:31:41 +0800

    2

--- chsi-processor/src/main/java/com/pwq/chsi/center/parser/GaokaoParser.java
+++ chsi-processor/src/main/java/com/pwq/chsi/center/parser/GaokaoParser.java
@@ -4,6 +4,7 @@ import com.gargoylesoftware.htmlunit.Page;
 import com.pwq.spider.chsi.common.CacheContainer;
 import com.pwq.spider.chsi.common.ProcessorCode;
 import com.pwq.spider.chsi.common.http.Result;
+import com.pwq.spider.chsi.common.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -25,7 +26,12 @@ public class GaokaoParser {
         List<Page> pages = cacheContainer.getPages(ProcessorCode.SPECIALITY_DETAIL.getCode());
         if(null != pages){
             for(Page page:pages){
-                String pageString = page.getWebResponse().getContentAsString();
+                String pageString = page.getWebResponse().getContentAsString().replaceAll("\\s+","");
+                String specialtyName = RegexUtils.matchValue("专业名称：</span>(.*?)<",pageString);//专业名称
+                String specialtyCode = RegexUtils.matchValue("专业代码：</span>(.*?)<",pageString);//专业代码
+                String ml = RegexUtils.matchValue("门类：</span>(.*?)<",pageString);//门类
+                String xk = RegexUtils.matchValue("学科：</span>(.*?)<",pageString);//学科
+                String educationLevel = RegexUtils.matchValue("学历层次：</span>(.*?)<",pageString);//学历层次
 
             }
         }
